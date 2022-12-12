03aabbf11574566ea0d51b6c88e16a96addf2f24
Marcin Luczak <v-Marcin.Luczak@lionbridge.com>
2022-12-12 15:22:28 +0100

    12 visualisation

--- 2022/12.cs
+++ 2022/12.cs
@@ -4,67 +4,113 @@
     {
         //static string args = "Sabqponm\r\nabcryxxl\r\naccszExk\r\nacctuvwj\r\nabdefghi";
         static string args = "abacccaaaacccccccccccaaaaaacccccaaaaaaccccaaacccccccccccccccccccccccccccccccccccccccccccaaaaa\r\nabaaccaaaacccccccccccaaaaaaccccccaaaaaaaaaaaaaccccccccccccccccccccccccccccccccccccccccccaaaaa\r\nabaaccaaaacccccccccccaaaaacccccaaaaaaaaaaaaaaaccccccccccccccccccccccccccccccccccccccccccaaaaa\r\nabccccccccccccccccccccaaaaacccaaaaaaaaaaaaaaaacccccccccccccccccccccccccccaaaccccccccccccaaaaa\r\nabccccccccccccccccccccaacaacccaaaaaaaaccaaaaaccccccccccccccccccccccccccccaaaccccccccccccaccaa\r\nabcccccccccccccaacccaaaccccccaaaaaaaaaccaaaaaccccccccccccccccccccccccccccccacccccccccccccccca\r\nabcccccccccccaaaaaaccaaaccacccccaaaaaaacccccccccccccccccccccccccciiiicccccccddddddccccccccccc\r\nabcccccccccccaaaaaaccaaaaaaaccccaaaaaacccccaacccccccaaaccccccccciiiiiiiicccdddddddddacaaccccc\r\nabccccccccccccaaaaaaaaaaaaacccccaaaaaaacaaaacccccccaaaacccccccchhiiiiiiiiicddddddddddaaaccccc\r\nabcccccccccccaaaaaaaaaaaaaacccccccaaacccaaaaaacccccaaaaccccccchhhipppppiiiijjjjjjjddddaaccccc\r\nabcccccccccccaaaaaaaaaaaaaaccccccccccccccaaaaaccccccaaaccccccchhhpppppppiijjjjjjjjjddeeaccccc\r\nabcccccccccccccccccaaaaaaaacccccccccccccaaaaaccccccccccccccccchhppppppppppjjqqqjjjjjeeeaacccc\r\nabccccccccccccccccccaaaaaaaacccccccccccccccaacccccccccccccccchhhpppuuuupppqqqqqqqjjjeeeaacccc\r\nabcccccccccccccccccccaacccacccccccccccccccccccccccccccccccccchhhopuuuuuuppqqqqqqqjjjeeecccccc\r\nabacccccccccccccaaacaaaccccccccccccccccccccccccccccaaccccccchhhhoouuuuuuuqvvvvvqqqjkeeecccccc\r\nabaccccccccccccaaaaaacccccaaccccccccccccccccccccccaaaccccccchhhooouuuxxxuvvvvvvqqqkkeeecccccc\r\nabaccccccccccccaaaaaacccaaaaaaccccccccccccccccccaaaaaaaaccchhhhooouuxxxxuvyyyvvqqqkkeeecccccc\r\nabcccccccccccccaaaaacccaaaaaaaccccccccccccccccccaaaaaaaaccjjhooooouuxxxxyyyyyvvqqqkkeeecccccc\r\nabccccccccccccccaaaaaacaaaaaaaccccccccaaaccccccccaaaaaaccjjjooootuuuxxxxyyyyyvvqqkkkeeecccccc\r\nabccccccccccccccaaaaaaaaaaaaacccccccccaaaacccccccaaaaaacjjjooootttuxxxxxyyyyvvrrrkkkeeecccccc\r\nSbccccccccccccccccccaaaaaaaaacccccccccaaaacccccccaaaaaacjjjoootttxxxEzzzzyyvvvrrrkkkfffcccccc\r\nabcccccccccccaaacccccaaaaaaacaaaccccccaaaccccccccaaccaacjjjoootttxxxxxyyyyyyvvvrrkkkfffcccccc\r\nabcccccccccaaaaaacccaaaaaacccaaacacccaacccccccccccccccccjjjoootttxxxxyxyyyyyywvvrrkkkfffccccc\r\nabcccccccccaaaaaacccaaaaaaaaaaaaaaaccaaacaaacccccaacccccjjjnnnttttxxxxyyyyyyywwwrrkkkfffccccc\r\nabcaacacccccaaaaacccaaacaaaaaaaaaaaccaaaaaaacccccaacaaacjjjnnnntttttxxyywwwwwwwwrrrlkfffccccc\r\nabcaaaaccccaaaaacccccccccaacaaaaaaccccaaaaaacccccaaaaacccjjjnnnnnttttwwywwwwwwwrrrrllfffccccc\r\nabaaaaaccccaaaaaccccccaaaaaccaaaaacaaaaaaaaccccaaaaaaccccjjjjinnnntttwwwwwsssrrrrrllllffccccc\r\nabaaaaaaccccccccccccccaaaaacaaaaaacaaaaaaaaacccaaaaaaacccciiiiinnnntswwwwssssrrrrrlllfffccccc\r\nabacaaaaccccccccccccccaaaaaacaaccccaaaaaaaaaaccccaaaaaaccccciiiinnnssswwsssssllllllllfffccccc\r\nabccaaccccccccccccccccaaaaaaccccccccccaaacaaaccccaaccaacccccciiiinnsssssssmmllllllllfffaacccc\r\nabccccccccccccccccccccaaaaaaccccccccccaaaccccccccaaccccccccccciiinnmsssssmmmmlllllgggffaacccc\r\nabcccccccccccccccaccccccaaacccccccccccaaccccccccccccccccccccccciiimmmsssmmmmmgggggggggaaacccc\r\nabcccccccccaaaaaaaaccccccccccccccccccccccccccccaaaaaccccccccccciiimmmmmmmmmgggggggggaaacccccc\r\nabccccccccccaaaaaaccccccccccccccccccaacccccccccaaaaacccccccccccciiimmmmmmmhhggggcaaaaaaaccccc\r\nabccccccccccaaaaaacccccccccccccccccaacccccccccaaaaaacccccccccccciihhmmmmhhhhgccccccccaacccccc\r\nabccccaacaaaaaaaaaaccccccccccccccccaaaccccccccaaaaaaccccccccccccchhhhhhhhhhhaaccccccccccccccc\r\nabccccaaaaaaaaaaaaaaccccccccccaaccaaaaccccccccaaaaaacccaaacccccccchhhhhhhhaaaaccccccccccccccc\r\nabcccaaaaaaaaaaaaaaaccccccccaaaaaacaaaacacaccccaaaccccaaaacccccccccchhhhccccaaccccccccccaaaca\r\nabcccaaaaaacacaaacccccccccccaaaaaaaaaaaaaaacccccccccccaaaacccccccccccaaaccccccccccccccccaaaaa\r\nabcccccaaaacccaaaccccccccccaaaaaaaaaaaaaaaaccccccccccccaaacccccccccccaaacccccccccccccccccaaaa\r\nabcccccaacccccaacccccccccccaaaaaaaaaaaaaccccccccccccccccccccccccccccccccccccccccccccccccaaaaa";
-
-        public static void Run()
+        static string _char = "*";
+        static string _backChar = "█";
+        public async static Task Run()
         {
-            int[][] map = args.Split("\r\n")
-                .Select(x => x.ToArray()
-                    .Select(y => Convert.ToInt32(y))
-                    .ToArray())
+            var map = args.Split("\r\n")
+                .Select(x => x.ToArray())
                 .ToArray();
 
-            var start = (0, 0, 0);
-            var end = (0, 0, 0);
+            var start = (0, 0, 0, (0, 0));
+            var end = (0, 0, 0, (0, 0));
             for (int i = 0; i < map.Length; i++)
             {
                 for (int j = 0; j < map[i].Length; j++)
                 {
                     if (map[i][j] == 'S')
                     {
-                        start = (i, j, 0);
+                        start = (i, j, 0, (0, 0));
                         map[i][j] = 'a';
                     }
                     else if (map[i][j] == 'E')
                     {
-                        end = (i, j, 0);
+                        end = (i, j, 0, (0, 0));
                         map[i][j] = 'z';
                     }
                 }
             }
 
-            List<(int, int, int)> paths = new List<(int, int, int)>
+            List<(int, int, int, (int, int))> steps = new List<(int, int, int, (int, int))>
             {
                 end
             };
 
-            for (int i = 0; i < paths.Count; i++)
+            await CounterDown();
+            Display(map, steps);
+
+            Console.SetCursorPosition(end.Item2, end.Item1 + 1);
+            Console.Write(_char);
+
+            for (int i = 0; i < steps.Count; i++)
             {
-                var current = paths[i];
+                var current = steps[i];
 
                 var x = current.Item1 - 1;
                 var y = current.Item2;
-                if (Move(map, start, paths, current, x, y)) break;
+                if (Move(map, start, steps, current, x, y)) break;
 
                 x = current.Item1 + 1;
                 y = current.Item2;
-                if (Move(map, start, paths, current, x, y)) break;
-
+                if (Move(map, start, steps, current, x, y)) break;
 
                 x = current.Item1;
                 y = current.Item2 - 1;
-                if (Move(map, start, paths, current, x, y)) break;
+                if (Move(map, start, steps, current, x, y)) break;
 
                 x = current.Item1;
                 y = current.Item2 + 1;
-                if (Move(map, start, paths, current, x, y)) break;
+                if (Move(map, start, steps, current, x, y)) break;
+
+                if (i % 10 == 0)
+                    await Task.Delay(1);
             }
 
-            Console.WriteLine(paths.Last().Item3);
+            backCounter = 0;
+            await GoBack(steps, (steps.Last().Item1, steps.Last().Item2));
+
+            Console.SetCursorPosition(0, map.Length + 2);
+            Console.WriteLine(steps.Last().Item3);
         }
 
-        static bool Move(int[][] map, (int, int, int) start, List<(int, int, int)> paths, (int, int, int) current, int x, int y)
+        static async Task CounterDown()
         {
-            //up down
+            for (int i = 3; i >= 0; i--)
+            {
+                Console.Clear();
+                Console.Write(i);
+                await Task.Delay(1000);
+            }
+
+            Console.Clear();
+        }
+
+        static int backCounter = 0;
+        static async Task GoBack(List<(int, int, int, (int, int))> steps, (int, int) current)
+        {
+            backCounter++;
+            if (backCounter % 5 == 0)
+                await Task.Delay(3);
+
+            if (current.Item1 == 0 && current.Item2 == 0)
+            {
+                return;
+            }
+
+            Console.SetCursorPosition(current.Item2, current.Item1 + 1);
+            Console.Write(_backChar);
+
+            var next = steps.Single(x => x.Item1 == current.Item1 && x.Item2 == current.Item2);
+            await GoBack(steps, next.Item4);
+        }
+
+        static bool Move(char[][] map,
+            (int, int, int, (int, int)) start,
+            List<(int, int, int, (int, int))> steps,
+            (int, int, int, (int, int)) current,
+            int x, int y)
+        {
+            //up down boundries
             if (x < 0 || x >= map.Length)
             {
                 return false;
@@ -81,13 +127,16 @@
                 map[x][y] + 1 == map[current.Item1][current.Item2] ||
                 map[x][y] > map[current.Item1][current.Item2])
             {
-                //exists
-                if (paths.Any(p => p.Item1 == x && p.Item2 == y))
+                //already exists
+                if (steps.Any(p => p.Item1 == x && p.Item2 == y))
                 {
                     return false;
                 }
 
-                paths.Add((x, y, current.Item3 + 1));
+                Console.SetCursorPosition(y, x + 1);
+                Console.Write(_char);
+
+                steps.Add((x, y, current.Item3 + 1, (current.Item1, current.Item2)));
                 if (map[x][y] == 97)
                 {
                     return true;
@@ -95,6 +144,18 @@
             }
 
             return false;
+        }
+
+        static void Display(char[][] map, List<(int, int, int, (int, int))> steps)
+        {
+            for (int i = 0; i < map.Length; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < map[i].Length; j++)
+                {
+                    Console.Write(map[i][j]);
+                }
+            }
         }
     }
 }
