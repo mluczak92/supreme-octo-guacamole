ce384d3a13c951666c924afeba99126974d3041d
Marcin Luczak <v-marcin.luczak@lionbridge.com>
2023-12-13 13:29:25 +0100

    12 part 2

--- 2023/12.cs
+++ 2023/12.cs
@@ -8,22 +8,22 @@ public class _12 {
         var input = File.ReadAllLines("input.txt")
             .Select(x => new
             {
-                Row = string.Join("u", new string(x.Split(" ")[0].ToArray()))
+                Row = string.Join("u", Enumerable.Repeat(0, 5).Select(_ => new string(x.Split(" ")[0].ToArray())))
                     .Replace("#", "d")
                     .Replace(".", "o")
                     .Replace("?", "u"),
-                BrokenList = string.Join(",", x.Split(" ")[1])
+                BrokenList = string.Join(",", Enumerable.Repeat(0, 5).Select(_ => string.Join(",", x.Split(" ")[1])))
                     .Split(",")
                     .Select(y => int.Parse(y))
                     .ToList()
             }).ToArray();
 
         var c = 0;
-        var sum = 0;
+        ulong sum = 0UL;
         foreach (var entry in input)
         {
             // Console.WriteLine(c++);
-            var cache = new Dictionary<(string, string), int>();
+            var cache = new Dictionary<(string, string), ulong>();
             sum += Replace(entry.Row, entry.BrokenList, -1, cache);
         }
         
@@ -31,12 +31,31 @@ public class _12 {
         Console.ReadKey();
         
         // 10154 - too high
+        
+        // part 2
+        // 2416287867 - too low
+        // 29826669191291
     }
 
     private static List<int> results = new();
 
-    static int Replace(string row, List<int> brokenList, int previousGroupIdx, Dictionary<(string, string), int> cache)
+    static ulong Replace(string row, List<int> brokenList, int previousGroupIdx, Dictionary<(string, string), ulong> cache)
     {
+        // CACHE
+        var key1 = row;
+        var lastDotIdx = row.LastIndexOf(".");
+        if (lastDotIdx > -1)
+        {
+            key1 = row.Substring(lastDotIdx + 1);
+        }
+
+        var key2 = string.Join(",", brokenList);
+        if (cache.TryGetValue((key1, key2), out var val))
+        {
+            // Console.WriteLine("CACHE HIT");
+            return val;
+        }
+        
         var number = brokenList.First();
         var pattern = $"(?=([d*u*d*u*]{{{number}}}[uo]))";
         
@@ -47,12 +66,12 @@ public class _12 {
 
         var matches = Regex.Matches(row, pattern);
         var groups = matches.Select(x => x.Groups.Values.Skip(1).First()).Where(x => x.Index > previousGroupIdx).ToArray();
-        Console.WriteLine($"trying to match: {new string(Enumerable.Range(0, number).Select(_ => 'd').ToArray())} in: {row}, matches: {groups.Length}");
+        // Console.WriteLine($"trying to match: {new string(Enumerable.Range(0, number).Select(_ => 'd').ToArray())} in: {row}, matches: {groups.Length}");
         
         var newList = new List<int>(brokenList.Skip(1));
         if (newList.Count == 0)
         {
-            var validGroupsCounter = 0;
+            ulong validGroupsCounter = 0;
             foreach (var g in groups)
             {
                 var newRow = row.Remove(g.Index, g.Length);
@@ -66,21 +85,24 @@ public class _12 {
             }
 
             // Console.WriteLine($"{row}: {validGroupsCounter}");
+            cache[(key1, key2)] = validGroupsCounter;
             return validGroupsCounter;
         }
 
-        int ways = 0;
+        ulong ways = 0;
         for (int i = 0; i < groups.Length; i++)
         {
             var group = groups.ElementAt(i);
             if (group.Index - 1 >= 0 && row[group.Index - 1] == 'd')
             {
+                cache[(key1, key2)] = ways;
                 return ways;
             }
             
             var newStart = group.Index + group.Length;
             if (newStart >= row.Length)
             {
+                cache[(key1, key2)] = ways;
                 return ways;
             }
 
@@ -89,18 +111,21 @@ public class _12 {
 
             if (newRow.IndexOf("d") > -1 && newRow.IndexOf("d") < group.Index)
             {
+                cache[(key1, key2)] = ways;
                 return ways;
             }
                 
             var count = newRow.Count(x => x == 'u' || x == 'd');
             if (count < newList.Sum(x => x))
             {
+                cache[(key1, key2)] = ways;
                 return ways;
             }
             
             ways += Replace(newRow, newList, group.Index, cache);
         }
 
+        cache[(key1, key2)] = ways;
         return ways;
     }
 }
