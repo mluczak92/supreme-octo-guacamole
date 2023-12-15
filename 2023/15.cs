c1ae28d2950b442d25bbfe881e838d874f9a958c
Marcin Luczak <v-marcin.luczak@lionbridge.com>
2023-12-15 19:01:42 +0100

    15 part 2

--- 2023/15.cs
+++ 2023/15.cs
@@ -1,26 +1,68 @@
-using System.Linq.Dynamic.Core;
+using System.Collections.Specialized;
 
 namespace adventofcode2022._2023;
 
 public class _15 {
     public static void Run()
     {
-        var sequence = File.ReadAllText("example.txt")
+        var sequence = File.ReadAllText("input.txt")
             .Split(",")
             .ToArray();
-        
-        var sum = 0ul;
+
+        Dictionary<int, OrderedDictionary> HASHMAP = new Dictionary<int, OrderedDictionary>();
         foreach (var step in sequence)
         {
-            sum += Hash(step);
+            var split = step.Split('=', '-');
+            var label = split[0];
+            
+            var key = Hash(label);
+            if (!HASHMAP.ContainsKey(key) && step.Contains('='))
+            {
+                HASHMAP.Add(key, new OrderedDictionary{ { label, int.Parse(split[1]) } });
+            }
+            else
+            {
+                if (step.Contains('-'))
+                {
+                    if (HASHMAP.TryGetValue(key, out var value))
+                    {
+                        value.Remove(label);
+                    }
+                }
+                else if (step.Contains('='))
+                {
+                    if (HASHMAP.TryGetValue(key, out var value))
+                    {
+                        value[label] = int.Parse(split[1]);
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+        }
+
+        var focusingPower = 0;
+        for (int i = 0; i < 256; i++)
+        {
+            if (!HASHMAP.ContainsKey(i))
+                continue;
+            
+            var innerDic = HASHMAP[i];
+            for (int j = 0; j < innerDic.Count; j++)
+            {
+                // Console.WriteLine($"{i + 1} * {j + 1} * {(int)innerDic[j]}");
+                focusingPower += (i + 1) * (j + 1) * (int)innerDic[j];
+            }
         }
         
-        Console.WriteLine(sum);
+        Console.WriteLine(focusingPower);
     }
 
-    public static ulong Hash(string input)
+    static int Hash(string input)
     {
-        var current = 0ul;
+        var current = 0;
         foreach (var @char in input)
         {
             current += @char;
