f1085769f06969f55bac24558381794254682e11
Marcin Luczak <v-marcin.luczak@lionbridge.com>
2024-12-06 12:27:43 +0100

    2024 6 started

---
+++ 2024/6/6.cs
@@ -0,0 +1,17 @@
+namespace adventofcode2022._2024._6;
+
+public static class _6 {
+    public static void Run_1()
+    {
+        var input = File.ReadAllLines("testArgs.txt");
+
+        Dictionary<char, char> rotateDicts =
+        {
+            { '^', '>' },
+            { '>', 'v' },
+            { 'v', '<' },
+            { '<', '^' }
+        };
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,12 @@
-﻿using adventofcode2022._2024;
+﻿using adventofcode2022._2024._6;
 
 namespace adventofcode2022
 {
     internal class Program
     {
-        // static async Task Main(string[] _)
-        // {
-        //     await _12.Run();
-        //
-        //     await Task.Delay(3000);
-        //     Console.Clear();
-        //
-        //     await _14.Run();
-        // }
-
         static void Main(string[] _)
         {
-            _5.Run_1();
-            _5.Run_2();
+            _6.Run_1();
         }
     }
 }
